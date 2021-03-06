fe4f1d209980f581e6b1be62baf7bd515cc0ceef
jbh3474 <joseph_b_harrell@homedepot.com>
2021-03-06 01:33:45 -0500

    updating build file with correct repo and tag name

--- cardano-node/build.sh
+++ cardano-node/build.sh
@@ -6,8 +6,8 @@ fi
 #docker login registry.gitlab.com
 #docker build -t registry.gitlab.com/viper-staking/docker-containers/cardano-node:$TAG .
 #docker push registry.gitlab.com/viper-staking/docker-containers/cardano-node:$TAG
-
+docker login
 docker build -t ehm-node:latest .
-docker tag ehm-node:latest bluealein56/ehm:ehm-node
+docker tag ehm-node:latest bluealein56/ehm-node:latest
 #docker push bluealein56/ehm:$TAG
-docker push bluealein56/ehm:ehm-node
+docker push bluealein56/ehm-node:latest
